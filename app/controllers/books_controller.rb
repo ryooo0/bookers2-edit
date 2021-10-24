59e9039f4cde03d9134c9e23bc65dc753428ade0
ryooookada <btbl70120@gmail.com>
2021-10-24 06:40:11 +0000

    [Add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,14 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -20,6 +22,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      flash[:notice] = "You have updated book successfully."
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @book = Book.new
     @users = User.all
+    @user = current_user
   end
   
   def show
@@ -13,6 +14,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to users_path
+    end
   end
   
   def create
@@ -22,6 +28,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
     redirect_to user_path(@user.id)
   end
   
