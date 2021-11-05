5acf7836f924864f770523771b48e39ff97cb56c
ryooookada <btbl70120@gmail.com>
2021-11-05 08:45:03 +0000

    [Add]bookers2-java

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  
+
   resources :books, only: [:edit, :create, :index, :show, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:edit, :index, :show, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
   get '/search' => 'searchs#search'
-  
+
 end
